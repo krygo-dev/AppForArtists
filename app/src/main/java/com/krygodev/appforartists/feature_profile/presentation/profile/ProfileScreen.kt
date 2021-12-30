d3f4df094c569a1ae72a842f2a5b6e515b61a376
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-12-30 21:37:32 +0100

    Default profile image, profile screen logout, edit, profile photo and username

--- app/src/main/java/com/krygodev/appforartists/feature_profile/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/krygodev/appforartists/feature_profile/presentation/profile/ProfileScreen.kt
@@ -1,18 +1,28 @@
 package com.krygodev.appforartists.feature_profile.presentation.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.ManageAccounts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import com.krygodev.appforartists.R
 import com.krygodev.appforartists.core.presentation.components.SetupBottomNavBar
+import com.krygodev.appforartists.core.presentation.util.Screen
 import com.krygodev.appforartists.core.presentation.util.UIEvent
 import kotlinx.coroutines.flow.collectLatest
 
@@ -47,13 +57,99 @@ fun ProfileScreen(
         modifier = Modifier.padding(horizontal = 8.dp),
         bottomBar = { SetupBottomNavBar(navController = navController) }
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Profile Screen")
-            Button(onClick = { viewModel.onEvent(ProfileEvent.SignOut) }) {
-                Text(text = "Wyloguj")
+        if (state.isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = Color.Black,
+                    strokeWidth = 5.dp
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 8.dp)
+            ) {
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        OutlinedButton(
+                            onClick = {
+                                navController.navigate(Screen.EditProfileScreen.route)
+                            },
+                            modifier = Modifier.size(40.dp),
+                            shape = CircleShape,
+                            contentPadding = PaddingValues(0.dp),
+                            colors = ButtonDefaults.outlinedButtonColors(
+                                contentColor = Color.LightGray,
+                                backgroundColor = Color.Black
+                            )
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.ManageAccounts,
+                                contentDescription = null,
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(4.dp))
+                        OutlinedButton(
+                            onClick = {
+                                viewModel.onEvent(ProfileEvent.SignOut)
+                            },
+                            modifier = Modifier.size(40.dp),
+                            shape = CircleShape,
+                            contentPadding = PaddingValues(0.dp),
+                            colors = ButtonDefaults.outlinedButtonColors(
+                                contentColor = Color.LightGray,
+                                backgroundColor = Color.Black
+                            )
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Logout,
+                                contentDescription = null,
+                            )
+                        }
+                    }
+                }
+                item {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(
+                                data = if (!userState.userPhotoUrl.isNullOrEmpty()) userState.userPhotoUrl
+                                    else R.mipmap.ic_default_profile_image,
+                                builder = {
+                                    transformations(CircleCropTransformation())
+                                }
+                            ),
+                            contentDescription = null,
+                            modifier = Modifier.size(100.dp)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "${userState.username}",
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.W700
+                        )
+                    }
+                }
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "${userState.bio}",
+                            fontSize = 20.sp
+                        )
+                    }
+                }
             }
         }
     }
