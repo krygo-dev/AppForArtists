12881f2413a2477ad1ef07fd31bc3ec10c9b68fa
Adam Chylaszek <adam.chylaszek98@gmail.com>
2022-01-09 11:48:31 +0100

    HomeViewModel impl

--- app/src/main/java/com/krygodev/appforartists/feature_image/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/krygodev/appforartists/feature_image/presentation/home/HomeScreen.kt
@@ -1,34 +1,101 @@
 package com.krygodev.appforartists.feature_image.presentation.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.krygodev.appforartists.core.presentation.components.SetupBottomNavBar
+import com.krygodev.appforartists.core.presentation.util.UIEvent
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun HomeScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val state = viewModel.state.value
+    val dailyImageState = viewModel.dailyImage.value
+    val mostLikedImages = viewModel.mostLikedImages.value
+    val bestRatedImages = viewModel.bestRatedImages.value
+    val recentlyAddedImages = viewModel.recentlyAddedImages.value
     val scaffoldState = rememberScaffoldState()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is UIEvent.ShowSnackbar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message
+                    )
+                }
+                is UIEvent.NavigateTo -> {
+                    navController.navigate(route = event.route)
+                }
+            }
+        }
+    }
 
     Scaffold(
         scaffoldState = scaffoldState,
         modifier = Modifier.padding(horizontal = 8.dp),
         bottomBar = { SetupBottomNavBar(navController = navController) }
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Home Screen")
+        if (state.isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = Color.Black,
+                    strokeWidth = 5.dp
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 8.dp)
+                    .padding(top = 8.dp)
+            ) {
+                item {
+
+                }
+                item {
+                    LazyRow {
+                        items(mostLikedImages) {
+
+                        }
+                    }
+                }
+                item {
+                    LazyRow {
+                        items(bestRatedImages) {
+
+                        }
+                    }
+                }
+                item {
+                    LazyRow {
+                        items(recentlyAddedImages) {
+
+                        }
+                    }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(70.dp))
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/krygodev/appforartists/feature_image/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/krygodev/appforartists/feature_image/presentation/home/HomeViewModel.kt
@@ -3,19 +3,24 @@ package com.krygodev.appforartists.feature_image.presentation.home
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.krygodev.appforartists.core.domain.model.ImageModel
+import com.krygodev.appforartists.core.domain.util.Resource
 import com.krygodev.appforartists.core.presentation.util.LoadingState
 import com.krygodev.appforartists.core.presentation.util.UIEvent
-import com.krygodev.appforartists.feature_image.domain.use_case.image_use_case.ImageUseCases
+import com.krygodev.appforartists.feature_image.domain.use_case.home_use_case.HomeUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val _imageUseCases: ImageUseCases
-) : ViewModel(){
+    private val _homeUseCases: HomeUseCases
+) : ViewModel() {
 
     private val _state = mutableStateOf(LoadingState())
     val state: State<LoadingState> = _state
@@ -23,6 +28,154 @@ class HomeViewModel @Inject constructor(
     private val _eventFlow = MutableSharedFlow<UIEvent>()
     val eventFlow = _eventFlow.asSharedFlow()
 
-    private val _images = mutableStateOf(listOf<ImageModel>())
-    val images: State<List<ImageModel>> = _images
+    private val _dailyImage = mutableStateOf(ImageModel())
+    val dailyImage: State<ImageModel> = _dailyImage
+
+    private val _mostLikedImages = mutableStateOf(listOf<ImageModel>())
+    val mostLikedImages: State<List<ImageModel>> = _mostLikedImages
+
+    private val _bestRatedImages = mutableStateOf(listOf<ImageModel>())
+    val bestRatedImages: State<List<ImageModel>> = _bestRatedImages
+
+    private val _recentlyAddedImages = mutableStateOf(listOf<ImageModel>())
+    val recentlyAddedImages: State<List<ImageModel>> = _recentlyAddedImages
+
+    init {
+        getDailyImage()
+        getMostLikedImages()
+        getBestRatedImages()
+        getRecentlyAddedImages()
+    }
+
+    private fun getDailyImage() {
+        viewModelScope.launch {
+            _homeUseCases.getRandomImage().onEach { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            error = "",
+                            result = null
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = "",
+                            result = result.data
+                        )
+
+                        _dailyImage.value = result.data!!
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = result.message!!,
+                            result = null
+                        )
+                        _eventFlow.emit(UIEvent.ShowSnackbar(result.message))
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
+
+    private fun getMostLikedImages() {
+        viewModelScope.launch {
+            _homeUseCases.getMostLikedImages(limit = 3).onEach { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            error = "",
+                            result = null
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = "",
+                            result = result.data
+                        )
+
+                        _mostLikedImages.value = result.data!!
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = result.message!!,
+                            result = null
+                        )
+                        _eventFlow.emit(UIEvent.ShowSnackbar(result.message))
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
+
+    private fun getBestRatedImages() {
+        viewModelScope.launch {
+            _homeUseCases.getBestRatedImages(limit = 3).onEach { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            error = "",
+                            result = null
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = "",
+                            result = result.data
+                        )
+
+                        _bestRatedImages.value = result.data!!
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = result.message!!,
+                            result = null
+                        )
+                        _eventFlow.emit(UIEvent.ShowSnackbar(result.message))
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
+
+    private fun getRecentlyAddedImages() {
+        viewModelScope.launch {
+            _homeUseCases.getRecentlyAddedImages(limit = 3).onEach { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            error = "",
+                            result = null
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = "",
+                            result = result.data
+                        )
+
+                        _recentlyAddedImages.value = result.data!!
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            error = result.message!!,
+                            result = null
+                        )
+                        _eventFlow.emit(UIEvent.ShowSnackbar(result.message))
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
 }
